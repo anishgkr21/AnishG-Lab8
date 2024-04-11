94a775a4b1a1d42eb999e64afedefa4d55f59afa
anishgkr21 <166748013+anishgkr21@users.noreply.github.com>
2024-04-11 15:25:46 -0400

    Anish G

--- main.py
+++ main.py
@@ -3,10 +3,27 @@
 
 
 def encode(password):
-    pass
+    encoded_password = ""
+    for i in password:
+        i += 3
+        i = str(i)
+        encoded_password += i
+    return encoded_password
+
+def decode(encoded_password):
+    decoded_password = encoded_password[::-1]
+    return decoded_password
 
 if __name__ == '__main__':
     while True:
-        break
+        print("Menu \n -------------- \n 1. Encode \n 2. Decode \n 3.Quit")
 
-    
+        choice = int(input("Please enter an option: "))
+
+        if choice == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print(encoded_password)
+
+
+
